175939192cb484364e187e2dd3d49b61956d7406
Deirdre Dunlea <deirdre.dunlea@cit.ie>
2017-06-13 15:50:54 +0100

    Made speculative changes

--- src/power.py
+++ src/power.py
@@ -2,16 +2,16 @@
 """
 
 def to_the_power(x,y =2)
-	result = x
-for i in range(0,y):
-	result = result * x
-return result
+	""" trying this out
+	"""
+	result x = y
+#	result = x
+#for i in range(0,y):
+#	result = result * x
+#return result
 
-x = 9
-<<<<<<< HEAD
+x = 5 #
 y = 8 #
-=======
-y = 8
->>>>>>> 48d1fc90269ae37889c57cfc4e3a44dd17e383b8
+
 print("{0} to the power of {1} is : {2} ".format(x,y, to_the_power(x,y)))	
 print("The End")
